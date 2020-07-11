0efeab0bae49fcca2238f080b94cb30e18c9fb80
Pavel Kudlanov <pkudlanov@yahoo.com>
2020-07-11 18:34:43 -0400

    url's and views for Post model Created the Post model specific views and wired up the url's for those views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from blog.forms import PostForm
+from .models import Post
+
 
 # Create your views here.
+class AboutView(generic.TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(generic.ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin, generic.CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, generic.UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, generic.ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
